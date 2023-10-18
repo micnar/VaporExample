ddf89a2f794e422fe2846b61b99e02d7bb3347c2
mic2 <michael@fam-michel.de>
2023-10-18 22:58:54 +0200

    Prepare migration for acronym table

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,30 @@
+//
+//  Acronym.swift
+//
+//
+//  Created by Michael Michel on 18.10.23.
+//
+
+import Foundation
+import Fluent
+
+final class Acronym: Model {
+    static let schema = "acronyms"
+    
+    @ID
+    var id: UUID?
+    
+    @Field(key: "short")
+    var short: String
+    
+    @Field(key: "long")
+    var long: String
+    
+    init() {}
+    
+    init(id: UUID? = nil, short: String, long: String) {
+        self.id = id
+        self.short = short
+        self.long = long
+    }
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,6 +16,11 @@ public func configure(_ app: Application) async throws {
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
+    
+    app.migrations.add(CreateAcronym())
+    app.logger.logLevel = .debug
+    
+    try app.autoMigrate().wait()
 
     // register routes
     try routes(app)
